ef81fa036dd420dba8a26e4c30250bd740f3a011
haileyabadeerx <haileykendramcduffie@gmail.com>
2025-04-07 16:48:28 +0800

    Add project files.

---
+++ Activity1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+public class GenshinArchons
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("<<Genshin Archon Finder>>");
+
+        string[] Archons = { "Barbatos - Venti", "Morax - Zhongli", "Beelzebub - Raiden Ei", "Buer - Nahida", "Focalors - Furina", "Murata - Mavuika", "Tsaritsa - Unknown" };
+
+        Console.WriteLine("\n1. Mondstadt \n2. Liyue \n3. Inazuma \n4. Sumeru \n5. Fontaine \n6. Natlan \n7. Snezhnaya\n");
+        Console.Write("Enter the number of the region you want to find: ");
+        //string Arch = Console.ReadLine();
+
+        if (int.TryParse(Console.ReadLine(), out int index))
+        {
+            if (index >= 0 && index <= Archons.Length)
+            {
+                Console.WriteLine($"\nThe Archon name is: {Archons[index-1]}");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid number. There are only 7 regions in Teyvat.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("\nInput out of bounds.");
+        }
+
+
+    }
+}
+
